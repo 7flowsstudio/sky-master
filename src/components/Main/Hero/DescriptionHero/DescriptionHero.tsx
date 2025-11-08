ff582267d89b1e1f311a18c5e23ff99d428289d4
7flowsstudio <seven.flows.studio@gmail.com>
2025-11-08 22:26:08 +0100

    add DescrHeroLink

--- src/components/Main/Hero/DescriptionHero/DescriptionHero.tsx
+++ src/components/Main/Hero/DescriptionHero/DescriptionHero.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import s from "./DescriptionHero.module.css";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
@@ -7,6 +7,7 @@ import { Link } from "@/i18n/routing";
 import StartTrainingHero from "../StartTrainingHero/StartTrainingHero";
 
 const DescriptionHero = () => {
+	const [hover, setHover] = useState(false);
 	const t = useTranslations("Hero");
 	return (
 		<ul className={s.descriptionList}>
@@ -14,7 +15,10 @@ const DescriptionHero = () => {
 				{t("descr_bottom_text_1")}
 				<span>{t("descr_bottom_text_2")}</span>
 			</li>
-			<li className={s.descrBaner}>
+			<li
+				className={s.descrBaner}
+				style={{ transform: hover ? "scale(1.02)" : "scale(1)" }}
+			>
 				<Image
 					src="/img/hero/soldier.png"
 					width={136}
@@ -25,7 +29,11 @@ const DescriptionHero = () => {
 				<ul className={s.linkBtnList}>
 					<li className={s.linkItem}>
 						<Link href="/programs">
-							<div className={s.linkIconWrap}>
+							<div
+								className={s.linkIconWrap}
+								onMouseEnter={() => setHover(true)}
+								onMouseLeave={() => setHover(false)}
+							>
 								<svg className={s.linkIcon}>
 									<use href="/sprite.svg#icon-arrow-top-right"></use>
 								</svg>
