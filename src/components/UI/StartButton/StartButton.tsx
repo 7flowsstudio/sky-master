4580422f2e2565b26857fd1e6c78e0a0223553a7
VitaliiBarabash1990 <alievruslan1818@gmail.com>
2025-11-25 13:10:30 +0100

    add modal in start button

--- src/components/UI/StartButton/StartButton.tsx
+++ src/components/UI/StartButton/StartButton.tsx
@@ -1,12 +1,16 @@
 "use client";
-import React from "react";
+import React, { SetStateAction } from "react";
 import s from "./StartButton.module.css";
 import { useTranslations } from "next-intl";
 
-const StartButton = () => {
+type SetOpenModalProps = {
+	setOpenModal: React.Dispatch<SetStateAction<boolean>>;
+};
+
+const StartButton: React.FC<SetOpenModalProps> = ({ setOpenModal }) => {
 	const t = useTranslations("");
 	return (
-		<button className={s.advBtn}>
+		<button className={s.advBtn} onClick={() => setOpenModal(true)}>
 			{t("CTA_BTN")}
 			<div className={s.iconWrapper}>
 				<svg className={s.iconSkyMaster}>
