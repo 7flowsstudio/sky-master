093311ea082555bcf5bf7df5ba5617d572b1a9c6
VitaliiBarabash1990 <alievruslan1818@gmail.com>
2025-11-25 09:52:13 +0100

    add minus in accordion

--- src/components/Main/FlightReadyAnswers/Accordion/Accordion.tsx
+++ src/components/Main/FlightReadyAnswers/Accordion/Accordion.tsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import s from "./Accordion.module.css";
 import { useTranslations } from "next-intl";
 import { accordionData } from "./accordionData";
 
 const Accordion = () => {
 	const [activeItem, setActiveItem] = useState<number | null>(null);
+	const [canAnimate, setCanAnimate] = useState(false);
+
+	useEffect(() => {
+		setCanAnimate(true);
+	}, []);
 	const t = useTranslations("Faq");
 
 	const accordionList = accordionData.map((item) => ({
@@ -28,8 +34,20 @@ const Accordion = () => {
 					<div className={s.accordionAnswer}>
 						<h4 className={s.questionTitle}>{item.question}</h4>
 						<div className={s.accordionIconWrapper}>
-							<svg className={s.accordionIcon}>
+							<svg
+								className={`${s.accordionIcon} ${
+									!canAnimate ? s.noAnimation : ""
+								} ${activeItem === item.id ? s.hidden : s.visible}`}
+							>
 								<use href="/sprite.svg#icon-step"></use>
+							</svg>
+
+							<svg
+								className={`${s.accordionIcon} ${
+									!canAnimate ? s.noAnimation : ""
+								} ${activeItem === item.id ? s.visible : s.hidden}`}
+							>
+								<use href="/sprite.svg#icon-step-minus"></use>
 							</svg>
 						</div>
 					</div>
